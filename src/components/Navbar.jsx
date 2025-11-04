459845383215142db9bbccf3595bf67efa80ea01
boyan-boyanov <b.i.boianov@gmail.com>
2025-11-04 15:30:15 +0200

    add customStyle.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,37 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
-import {styles} from '../styles'
-import {navLinks} from '../constants'
-import {logo, menu, close} from '../assets'
+import { styles } from '../styles'
+import { navLinks } from '../constants'
+import { logo, menu, close } from '../assets'
 import Logo2 from '../assets/Logo2.png'
 
 const Navbar = () => {
   const [active, setActive] = useState("")
+  console.log(active);
+  console.log(navLinks);
+  
+  
   return (
-   <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-    <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-      <Link to="/" className='flex items-center gap-2' ocClick={() => {
-        setActive("")
-        window.scroll(0,0)
-      }} >
-      <img src={Logo2} className='w-9 h-9 object-contain'/>
-      <p className='text-white text-[18px] font-bold cursor-pointer'>Boyan <span className='sm:block hidden'>| javaScript Mastery</span></p>
-      </Link>
-    </div>
-   </nav>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link to="/" className='flex items-center gap-2' ocClick={() => {
+          setActive("")
+          window.scroll(0, 0)
+        }} >
+          <img src={Logo2} className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Boyan <span className='sm:block hidden'>| javaScript Mastery</span></p>
+        </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((link) => (
+            <li key={link.id} className={`${active === link.title ? "custom-white" : "custom-secondary"} 
+            text-[18px] font-medium cursor-pointer nav-links`} onClick={() => setActive(link.title)}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
   )
 }
 
