729ca07ade12ba02d6fb75a00a4376b35b408041
boyan-boyanov <b.i.boianov@gmail.com>
2025-11-04 16:18:25 +0200

    fix navbar css with custom css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,25 +2,28 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { styles } from '../styles'
+import './customStyles/Navbar.css'
 import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 import Logo2 from '../assets/Logo2.png'
 
 const Navbar = () => {
   const [active, setActive] = useState("")
+  const [toggle, setToggle] = useState(false)
   console.log(active);
   console.log(navLinks);
-  
-  
+
+
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+    <nav className='navbar'>
+      <div className='navigation-container'>
         <Link to="/" className='flex items-center gap-2' ocClick={() => {
           setActive("")
           window.scroll(0, 0)
         }} >
           <img src={Logo2} className='w-9 h-9 object-contain' />
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Boyan <span className='sm:block hidden'>| javaScript Mastery</span></p>
+          <p className='custom-white text-[18px] font-bold cursor-pointer flex'>Boyan &nbsp;
+            <span className='sm:block hidden'>| JS Mastery</span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
@@ -30,6 +33,22 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle(!toggle)} />
+          <div className={`${toggle ? "flex" : "hidden"} mobile-menu`}>
+            <ul className='list-none flex justify-end items-start flex-col gap-4'>
+              {navLinks.map((link) => (
+                <li key={link.id} className={`${active === link.title ? "custom-white" : "custom-secondary"} 
+                    text-[16px] font-medium cursor-pointer font-poppins`} onClick={() => {
+                      setToggle(false)
+                      setActive(link.title)
+                      }}>
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
